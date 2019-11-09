82e76d454d5cce3b1a4c46662d0ef618e6a81a8b
RohanGautam <rohan1gautam@gmail.com>
2019-11-09 22:12:53 +0800

    multiple seat booking, lower while loop

--- src/Booking/SeatSelector.java
+++ src/Booking/SeatSelector.java
@@ -1,10 +1,36 @@
 package Booking;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SeatSelector {
 	public String choice;
+	private ArrayList<String> seatsChosenList;
 	
+	public SeatSelector(ArrayList<String> seatsChosenList) {
+		this.seatsChosenList =seatsChosenList; 
+	}
+	
+//	public void showSeats() {
+//		Integer rows = 10, cols = 10;
+//		Integer A = 65, Z = 90;
+//		// print column listing
+//		System.out.print("\t");
+//		for (Integer i = 0; i < cols; i += 1) {
+//			System.out.print(i.toString() + "\t");
+//		}
+//		System.out.println();
+//		// print row listing
+//		for (int i = A; (i < A + rows) && (A + rows <= Z); i++) {
+//			System.out.print(Character.toString((char) i) + "\t");
+//			for (int j = 0; j < cols; j += 1) {
+//				System.out.print("[]" + "\t");
+//			}
+//			System.out.println();
+//		}
+//	}
+
+	// function overloading for showSeats [diff parameter]
 	public void showSeats() {
 		Integer rows = 10, cols = 10;
 		Integer A = 65, Z = 90;
@@ -18,34 +44,15 @@ public class SeatSelector {
 		for (int i = A; (i < A + rows) && (A + rows <= Z); i++) {
 			System.out.print(Character.toString((char) i) + "\t");
 			for (int j = 0; j < cols; j += 1) {
-				System.out.print("[]" + "\t");
-			}
-			System.out.println();
-		}
-	}
-
-	// function overloading for showSeats [diff parameter]
-	public void showSeats(String choice) {
-		int rowChoice = (int) choice.charAt(0);
-		int colChoice = Character.getNumericValue(choice.charAt(1));
-
-		Integer rows = 10, cols = 10;
-		Integer A = 65, Z = 90;
-		// print column listing
-		System.out.print("\t");
-		for (Integer i = 0; i < cols; i += 1) {
-			System.out.print(i.toString() + "\t");
-		}
-		System.out.println();
-		// print row listing
-		for (int i = A; (i < A + rows) && (A + rows <= Z); i++) {
-			System.out.print(Character.toString((char) i) + "\t");
-			for (int j = 0; j < cols; j += 1) {
-				if (i == rowChoice && j == colChoice) {
-					System.out.print("[*]" + "\t");
-				} else {
-					System.out.print("[]" + "\t");
+				for (String choice : this.seatsChosenList) {
+					int rowChoice = (int) choice.charAt(0);
+					int colChoice = Character.getNumericValue(choice.charAt(1));
+					if (i == rowChoice && j == colChoice) {
+						System.out.print("[*]" + "\t");
+						j+=1;
+					}
 				}
+				System.out.print("[]" + "\t");
 			}
 			System.out.println();
 		}
@@ -70,11 +77,15 @@ public class SeatSelector {
 				choice = sc.next();
 				choice = choice.toUpperCase();
 			}
-			showSeats(choice);
+			this.seatsChosenList.add(choice);
+			showSeats();
 			System.out.println("Confirm? (y/n):");
 			String cfm = sc.next();
 			if (cfm.toLowerCase().equals("y")) {
 				break;
+			}
+			else {
+				this.seatsChosenList.remove(this.seatsChosenList.size()-1);
 			}
 			sc.close();
 		}
