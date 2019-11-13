ff1418a724f129bac9b72fc67aeca36417a1dbae
RohanGautam <rohan1gautam@gmail.com>
2019-11-13 22:37:13 +0800

    Revert "Edited the price changing functions, so that the changes are reflected through the entire application"
    This reverts commit 58f8837ccc2e269de41b264835d787d6b201159f.

--- src/Admin/TicketPrice.java
+++ src/Admin/TicketPrice.java
@@ -1,17 +1,12 @@
 package Admin;
 
 import java.util.Scanner;
-import General.CinemaTypes;
-import General.MovieTypes;
-import General.AgeTypes;
-import General.DayTypes;
-import Main;
 
-TicketPrice {
-	/*private Double[] typecinema;
+public class TicketPrice {
+	private Double[] typecinema;
 	private Double[] typemovie;
 	private Double[] typeage;
-	private Double[] typeday;*/
+	private Double[] typeday;
 	private int user_choice;
 	private double new_price1;
 	private int cinema_choice = 0;
@@ -19,134 +14,132 @@ TicketPrice {
 
 	Scanner sc = new Scanner(System.in);
 
-	/*
 	public TicketPrice() {
 		typecinema = new Double[] { 10.0, 12.0, 15.0 };
 		typemovie = new Double[] { 4.0, 5.0, 6.0 };
 		typeage = new Double[] { 2.0, 3.0, 4.0 };
 		typeday = new Double[] { 4.0, 5.0, 6.0 };
 
-	}*/
+	}
 
 	public void tpmovietype() {
-		// double twod = 4.00, threed = 5.00, bb = 6.00;
+		double twod = 4.00, threed = 5.00, bb = 6.00;
 		int movietype_choice = 0;
 		System.out.println("Which movie type:");
 		System.out.println("1.2-D  2.3-D 3.Blockbuster ");
 		movietype_choice = sc.nextInt();
 		switch (movietype_choice) {
 		case 1:
-			System.out.println("Current price is: "+ moviePriceMap.get(MovieTypes.TWOD)+", Enter the 2D ticket price: ");
-			moviePriceMap.put(MovieTypes.TWOD, sc.nextDouble());
-			// The method put will replace the value of an existing key and will create it if doesn't exist
+			System.out.println("Current price is: "+twod+", Enter the 2D ticket price: ");
+			twod = sc.nextDouble();
 			break;
 
 		case 2:
-			System.out.println("Current price is: "+ moviePriceMap.get(MovieTypes.THREED)+", Enter the 3D ticket price: ");
-			moviePriceMap.put(MovieTypes.THREED, sc.nextDouble());
+			System.out.println("Current price is: "+threed+", Enter the 3D ticket price: ");
+			threed = sc.nextDouble();
 			break;
 
 		case 3:
-			System.out.println("Current price is: "+ moviePriceMap.get(MovieTypes.BLOCKBUSTER)+", Enter the blockbuster ticket price: ");
-			moviePriceMap.put(MovieTypes.BLOCKBUSTER, sc.nextDouble());
+			System.out.println("Current price is: "+bb+", Enter the blockbuster ticket price: ");
+			bb = sc.nextDouble();
 			break;
 		}
-		/*typemovie[0] = twod;
+		typemovie[0] = twod;
 		typemovie[1] = threed;
 		typemovie[2] = bb;
-
 		for(Double x : typemovie) {
 			System.out.print(x.toString()+" ");
-		}*/
-		for(MovieTypes theType: MovieTypes.values()){
-        	System.out.println(theType.name() = moviePriceMap.get(theType));
-        }
- 
+		}
 		System.out.println();
 	}
 
 	public void tpcinema() {
-		//double GoldExtra = 12.00, SilverExtra = 10.00, PlatinumExtra = 15.00;
+		double GoldExtra = 12.00, SilverExtra = 10.00, PlatinumExtra = 15.00;
 
 		System.out.println("Which cinema type:");
 		System.out.println("1.Silver  2.Gold  3.Platinum ");
 		cinema_choice = sc.nextInt();
 		switch (cinema_choice) {
 		case 1:
-			System.out.println("Current price is: "+ cinemaPriceMap.get(CinemaTypes.SILVER)+", Enter the silver ticket price: ");
-			cinemaPriceMap.put(CinemaTypes.SILVER, sc.nextDouble());
+			System.out.println("Current price is: "+SilverExtra+", Enter the silver extra ticket price: ");
+			SilverExtra = sc.nextDouble();
 			break;
 
 		case 2:
-			System.out.println("Current price is: "+ cinemaPriceMap.get(CinemaTypes.GOLD)+", Enter the gold ticket price: ");
-			cinemaPriceMap.put(CinemaTypes.GOLD, sc.nextDouble());
+			System.out.println("Current price is: "+GoldExtra+", Enter the gold extra ticket price: ");
+			GoldExtra = sc.nextDouble();
 			break;
 
 		case 3:
-			System.out.println("Current price is: "+ cinemaPriceMap.get(CinemaTypes.PLATINUM)+", Enter the platinum ticket price: ");
-			cinemaPriceMap.put(CinemaTypes.PLATINUM, sc.nextDouble());
+			System.out.println("Current price is: "+PlatinumExtra+", Enter the platinum extra ticket price: ");
+			PlatinumExtra = sc.nextDouble();
 			break;
 		}
-		for(CinemaTypes theType: CinemaTypes.values()){
-        	System.out.println(theType.name() = cinemaPriceMap.get(theType));
-        }
- 
+		typecinema[0] = SilverExtra;
+		typecinema[1] = GoldExtra;
+		typecinema[2] = PlatinumExtra;
+		for(Double x : typecinema) {
+			System.out.print(x.toString()+" ");
+		}
 		System.out.println();
 	}
 
 	public void agecinema() {
-		//double student = 4.00, senior = 3.00, child = 2.00;
+		double student = 4.00, senior = 3.00, child = 2.00;
 		int age_choice = 0;
 		System.out.println("Which age group:");
 		System.out.println("1.Child 2.Senior 3.Student");
 		age_choice = sc.nextInt();
 		switch (age_choice) {
 		case 1:
-			System.out.println("Current price is: "+ agePriceMap.get(AgeTypes.CHILD)+", Enter the child ticket price: ");
-			agePriceMap.put(AgeTypes.CHILD, sc.nextDouble());
+			System.out.println("Current price is: "+child+", Enter the Child price: ");
+			student = sc.nextDouble();
 			break;
 
 		case 2:
-			System.out.println("Current price is: "+ agePriceMap.get(AgeTypes.SENIOR)+", Enter the senior ticket price: ");
-			agePriceMap.put(AgeTypes.SENIOR, sc.nextDouble());
+			System.out.println("Current price is: "+senior+", Enter the Senior price: ");
+			senior = sc.nextDouble();
 			break;
 
 		case 3:
-			System.out.println("Current price is: "+ agePriceMap.get(AgeTypes.STUDENT+", Enter the student ticket price: ");
-			agePriceMap.put(AgeTypes.STUDENT, sc.nextDouble());
+			System.out.println("Current price is: "+student+", Enter the Student price: ");
+			child = sc.nextDouble();
 			break;
 		}
-		for(AgeTypes theType: AgeTypes.values()){
-        	System.out.println(theType.name() = agePriceMap.get(theType));
-        }
- 
+		typeage[0] = child;
+		typeage[1] = senior;
+		typeage[2] = student;
+		for(Double x : typeage) {
+			System.out.print(x.toString()+" ");
+		}
 		System.out.println();
 	}
 
 	public void daycinema() {
-		//double weekdays = 4.00, weekends = 5.00, holidays = 6.00;
+		double weekdays = 4.00, weekends = 5.00, holidays = 6.00;
 		int day_choice = 0;
 		System.out.println("Which day:");
 		System.out.println("1.Weekdays 2.Weekends 3.Holidays");
 		day_choice = sc.nextInt();
 		switch (day_choice) {
 		case 1:
-			System.out.println("Current price is: "+ dayPriceMap.get(DayTypes.WEEKDAY)+", Enter the weekday ticket price: ");
-			dayPriceMap.put(DayTypes.WEEKDAY, sc.nextDouble());
-			break;
+			System.out.println("Current price is: "+weekdays+", Enter the Weekdays price: ");
+			weekdays = sc.nextDouble();
+
 		case 2:
-			System.out.println("Current price is: "+ dayPriceMap.get(DayTypes.WEEKEND)+", Enter the weekend ticket price: ");
-			dayPriceMap.put(DayTypes.WEEKEND, sc.nextDouble());
-			break;
+			System.out.println("Current price is: "+weekends+", Enter the Weekends price: ");
+			weekends = sc.nextDouble();
+
 		case 3:
-			System.out.println("Current price is: "+ dayPriceMap.get(DayTypes.HOLIDAY)+", Enter the holiday ticket price: ");
-			dayPriceMap.put(DayTypes.HOLIDAY, sc.nextDouble());
-			break;
+			System.out.println("Current price is: "+holidays+", Enter the Holidays price: ");
+			holidays = sc.nextDouble();
 		}
-		for(DayTypes theType: DayTypes.values()){
-        	System.out.println(theType.name() = dayPriceMap.get(theType));
-        }
- 
+		typeday[0] = weekdays;
+		typeday[1] = weekends;
+		typeday[2] = holidays;
+		for(Double x : typeday) {
+			System.out.print(x.toString()+" ");
+		}
 		System.out.println();
 	}
 }
